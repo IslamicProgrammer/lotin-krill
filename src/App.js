bff24bced1996fe99b26e9be8e2cffc70ac15277
Murodjon Azamov <63091703+IslamicProgrammer@users.noreply.github.com>
2023-11-18 23:23:48 +0500

    bug fixes and optimization

--- src/App.js
+++ src/App.js
@@ -1,6 +1,40 @@
 import React, { useState, useRef } from "react";
 import "./App.css";
 
+const ALPHABET_MAPPING = {
+  a: "а",
+  b: "б",
+  c: "с",
+  d: "д",
+  e: "е",
+  f: "ф",
+  g: "г",
+  h: "х",
+  i: "и",
+  j: "ж",
+  k: "к",
+  l: "л",
+  m: "м",
+  n: "н",
+  o: "о",
+  p: "п",
+  q: "қ",
+  r: "р",
+  s: "с",
+  t: "т",
+  u: "у",
+  v: "в",
+  x: "х",
+  y: "й",
+  z: "з",
+  "'": "",
+  ch: "Ч",
+  sh: "ш",
+  ye: "е",
+  ya: "я",
+  yo: "ё",
+};
+
 function App() {
   const [text, setText] = useState("");
   const [icon, setIcon] = useState("fas fa-copy");
@@ -8,192 +42,47 @@ function App() {
   const resultRef = useRef(null);
 
   const setTextHandler = (e) => {
-    const result = e.target.value.split("");
-    result.forEach((sign, index) => {
-      if (result[index] === "o" && result[index + 1] === "'") {
-        result[index] = "";
-        result[index + 1] = "ў";
-      }
-      if (result[index] === "g" && result[index + 1] === "'") {
-        result[index] = "";
-        result[index + 1] = "ғ";
-      }
-      if (result[index] === "s" && result[index + 1] === "h") {
-        result[index] = "";
-        result[index + 1] = "ш";
-      }
-      if (result[index] === "c" && result[index + 1] === "h") {
-        result[index] = "";
-        result[index + 1] = "ч";
-      }
-    });
-    result.forEach((sign, index) => {
-      if (sign == sign.toLowerCase()) {
-        switch (sign) {
-          case "a":
-            result[index] = "a";
-            break;
-          case "b":
-            result[index] = "б";
-            break;
-          case "c":
-            result[index] = "c";
-            break;
-          case "d":
-            result[index] = "д";
-            break;
-          case "e":
-            result[index] = "e";
-            break;
-          case "f":
-            result[index] = "ф";
-            break;
-          case "g":
-            result[index] = "г";
-            break;
-          case "h":
-            result[index] = "х";
-            break;
-          case "i":
-            result[index] = "и";
-            break;
-          case "j":
-            result[index] = "ж";
-            break;
-          case "k":
-            result[index] = "к";
-            break;
-          case "l":
-            result[index] = "л";
-            break;
-          case "m":
-            result[index] = "м";
-            break;
-          case "n":
-            result[index] = "н";
-            break;
-          case "o":
-            result[index] = "о";
-            break;
-          case "p":
-            result[index] = "п";
-            break;
-          case "q":
-            result[index] = "қ";
-            break;
-          case "r":
-            result[index] = "р";
-            break;
-          case "s":
-            result[index] = "с";
-            break;
-          case "t":
-            result[index] = "т";
-            break;
-          case "u":
-            result[index] = "у";
-            break;
-          case "v":
-            result[index] = "в";
-            break;
-          case "x":
-            result[index] = "х";
-            break;
-          case "y":
-            result[index] = "й";
-            break;
-          case "z":
-            result[index] = "з";
-            break;
-        }
+    const inputText = e.target.value.toLowerCase();
+    let result = "";
+
+    for (let i = 0; i < inputText.length; i++) {
+      const currentChar = inputText[i];
+      const nextChar = i < inputText.length - 1 ? inputText[i + 1] : null;
+
+      if (ALPHABET_MAPPING[currentChar]) {
+        result += ALPHABET_MAPPING[currentChar];
+      } else if (currentChar === "o" && nextChar === "'") {
+        result += "ў";
+        i++; // skip the next character
+      } else if (currentChar === "g" && nextChar === "'") {
+        result += "ғ";
+        i++; // skip the next character
+      } else if (currentChar === "s" && nextChar === "h") {
+        result += "сх";
+        i++; // skip the next character
+      } else if (currentChar === "c" && nextChar === "h") {
+        result += "ч";
+        i++; // skip the next character
+      } else if (currentChar === "y" && nextChar === "e") {
+        result += "е";
+        i++; // skip the next character
+      } else if (currentChar === "y" && nextChar === "a") {
+        result += "я";
+        i++; // skip the next character
+      } else if (currentChar === "y" && nextChar === "o") {
+        result += "ё";
+        i++; // skip the next character
       } else {
-        switch (sign.toLowerCase()) {
-          case "a":
-            result[index] = "a".toUpperCase();
-            break;
-          case "b":
-            result[index] = "б".toUpperCase();
-            break;
-          case "c":
-            result[index] = "c".toUpperCase();
-            break;
-          case "d":
-            result[index] = "д".toUpperCase();
-            break;
-          case "e":
-            result[index] = "e".toUpperCase();
-            break;
-          case "f":
-            result[index] = "ф".toUpperCase();
-            break;
-          case "g":
-            result[index] = "г".toUpperCase();
-            break;
-          case "h":
-            result[index] = "х".toUpperCase();
-            break;
-          case "i":
-            result[index] = "и".toUpperCase();
-            break;
-          case "j":
-            result[index] = "ж".toUpperCase();
-            break;
-          case "k":
-            result[index] = "к".toUpperCase();
-            break;
-          case "l":
-            result[index] = "л".toUpperCase();
-            break;
-          case "m":
-            result[index] = "м".toUpperCase();
-            break;
-          case "n":
-            result[index] = "н".toUpperCase();
-            break;
-          case "o":
-            result[index] = "о".toUpperCase();
-            break;
-          case "p":
-            result[index] = "п".toUpperCase();
-            break;
-          case "q":
-            result[index] = "қ".toUpperCase();
-            break;
-          case "r":
-            result[index] = "р".toUpperCase();
-            break;
-          case "s":
-            result[index] = "с".toUpperCase();
-            break;
-          case "t":
-            result[index] = "т".toUpperCase();
-            break;
-          case "u":
-            result[index] = "у".toUpperCase();
-            break;
-          case "v":
-            result[index] = "в".toUpperCase();
-            break;
-          case "x":
-            result[index] = "х".toUpperCase();
-            break;
-          case "y":
-            result[index] = "й".toUpperCase();
-            break;
-          case "z":
-            result[index] = "з".toUpperCase();
-            break;
-        }
+        result += currentChar;
       }
+    }
 
-      setIcon("fas fa-copy");
-    });
-
-    setText(result.join(""));
+    setIcon("fas fa-copy");
+    setText(result);
   };
 
   function copyDivToClipboard() {
-    var range = document.createRange();
+    const range = document.createRange();
     range.selectNode(resultRef.current);
     window.getSelection().removeAllRanges();
     window.getSelection().addRange(range);
